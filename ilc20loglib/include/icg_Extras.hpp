2119fcea31d7d933a6afb319b3241a6277e97419
WhaGames <130051824+WhaGames@users.noreply.github.com>
2023-10-30 17:21:09 +0100

    Add files via upload
    pre-defined function for easy implementation

---
+++ ilc20loglib/include/icg_Extras.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#define ilc20_notObbligate_cookie = 0
+#define ilc20_TRUE true
+#define ilc20_FALSE false
+#define ilc20_ELIF else if 
+#define ilc20_ELSE else
+#define ilc20_IF if
+#define ilc20_EMajor =>
+#define ilc20_EMinor <=
+#define ilc20_Major >
+#define ilc20_Minor <
+#define ilc20_And &&
+#define ilc20_or or
+#define ilc20_While while
+#define ilc20_void void
+#define ilc20_int int
+#define ilc20_bool bool
+#define ilc20_double double
+#define ilc20_float float
+
+struct ilc20_vec2 {
+	int x;
+	int y;
+};
